e551adfb6b267b1744c5dbaa6410486e3ed46e35
Jerome Bakker <jeabakker@coldtrick.com>
2013-07-31 17:22:33 +0200

    changed: access exception for thewire

--- views/default/input/access.php
+++ views/default/input/access.php
@@ -33,7 +33,7 @@ if ($vars['value'] == ACCESS_DEFAULT) {
 	$vars['value'] = get_default_access();
 }
 
-if (!elgg_in_context("admin") && !elgg_in_context("widgets")) {
+if (!elgg_in_context("admin") && !elgg_in_context("widgets") && !elgg_in_context("thewire")) {
 	$value = elgg_extract("value", $vars);
 	
 	$access_id_string = get_readable_access_level($value);
