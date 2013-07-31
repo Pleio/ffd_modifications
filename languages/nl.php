38c12cc0e5d98aea93533f7c103b81c48c9e10f5
Jerome Bakker <jeabakker@coldtrick.com>
2013-07-31 15:31:21 +0200

    dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,13 @@
+<?php
+
+	$dutch = array(
+		// settings
+		'ffd_modifications:settings:fix_access' => "Repareer content toegang",
+		'ffd_modifications:settings:fix_access:description' => "We hebben %s content items gevonden die niet de toegang '%s' hebben, klik op de onderstaande knop om dit te repareren.",
+		
+		// actions
+		// fix access
+		'ffd_modifications:action:fix_access' => "De toegang van %s content items is veranderd",
+	);
+	
+	add_translation("nl", $dutch);
